5f4aa0ac15c998b51679dfa9281ee1f58127b50e
puchkovki <puchkov.k@phystech.edu>
2020-04-01 13:51:45 +0300

    Added scrypt for the graphic building

--- integral/build_time.sh
+++ integral/build_time.sh
@@ -3,7 +3,7 @@ FULLDIR=$(dirname "$0")
 mpicc -Wall -Wextra -pedantic -O3 -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wcast-qual \
 -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -fsanitize=undefined \
 -fno-sanitize-recover=all -fstack-protector -o $FULLDIR/test $FULLDIR/graph.c -lm || exit 1
-for ((i = 1; i <= 24; ++i))
+for ((i = 1; i <= 28; ++i))
 do
 mpirun -np $i $FULLDIR/test $1 $2 >> 3.txt
 done
---
+++ integral/res/data.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int size = 0;
+    double time = 1, tmp = 0;
+
+    FILE* data = fopen("res/data.txt", "rt");
+    FILE* acceleration = fopen("res/acceleration.txt", "w");
+    FILE* efficiency = fopen("res/efficiency.txt", "w");
+    if(!data) {
+        printf("Error open data.txt\n");
+        return EXIT_FAILURE;
+    }
+    if(!acceleration) {
+        printf("Error open acceleration.txt\n");
+        return EXIT_FAILURE;
+    } 
+    while(!feof(data)) {
+        if(fscanf(data,"%d %lf\n", &size, &time) != 0) {
+            if(size == 1) {
+                tmp = time;
+            }
+            fprintf(acceleration, "%d %lf\n", size, tmp / time);
+            fprintf(efficiency, "%d %lf\n", size, tmp / time / size);
+        }
+    }
+    fclose(data);
+    fclose(acceleration);
+    fclose(efficiency);
+
+    return EXIT_SUCCESS;
+}
